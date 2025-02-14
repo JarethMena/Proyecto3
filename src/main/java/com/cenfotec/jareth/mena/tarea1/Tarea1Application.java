a1b44a4f918656947a40ec90efba5f9ddf03dc36
Jareth Mena <menaleitonj@outlook.com>
2025-02-13 20:50:39 -0600

    moving to repo root

---
+++ src/main/java/com/cenfotec/jareth/mena/tarea1/Tarea1Application.java
@@ -0,0 +1,12 @@
+package com.cenfotec.jareth.mena.tarea1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tarea1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Tarea1Application.class, args);
+    }
+}
